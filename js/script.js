b4e77e6282366291bb270d16f1a41753c2b3cea2
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-03-31 15:41:33 +0100

    SLight changes to css

--- js/script.js
+++ js/script.js
@@ -7,19 +7,23 @@ let buttons = document.querySelectorAll(".button");
 let newGameBtn = document.getElementById("newBtn");
 let scoreEl1 = document.querySelector(".human-score");
 let scoreEl2 = document.querySelector(".computer-score");
+let humanBg = document.querySelector(".human-player");
+
+scoreEl1.textContent = 0;
+scoreEl2.textContent = 0;
 
 // Function for human selection
 function humanSelection(button) {
   // alert("Button clicked");
   if (button === rockEl) {
     //   alert("Rock element clicked");
-    selectedIcon = '<i class="fas fa-hand-rock fa-3x"></i>'; // Rock icon
+    selectedIcon = '<i class="fas fa-hand-rock"></i>'; // Rock icon
   } else if (button === paperEl) {
     //   alert("Paper element clicked");
-    selectedIcon = '<i class="fas fa-hand-paper fa-3x"></i>';
+    selectedIcon = '<i class="fas fa-hand-paper"></i>';
   } else {
     //   alert("Scissors element clicked");
-    selectedIcon = '<i class="fas fa-hand-scissors fa-3x"></i>'; // Scissors icon
+    selectedIcon = '<i class="fas fa-hand-scissors"></i>'; // Scissors icon
   }
   humanImg.innerHTML = `${selectedIcon}`;
 }
@@ -33,17 +37,17 @@ function computerSelection() {
     // '<i class="fas fa-hand-scissors fa-3x"></i>', // Scissors
     {
       name: "Rock",
-      icon: `<i class="fas fa-hand-rock fa-3x"></i>`,
+      icon: `<i class="fas fa-hand-rock"></i>`,
     },
 
     {
       name: "Paper",
-      icon: `<i class="fas fa-hand-paper fa-3x"></i>`,
+      icon: `<i class="fas fa-hand-paper"></i>`,
     },
 
     {
       name: "Scissors",
-      icon: `<i class="fas fa-hand-scissors fa-3x"></i>`,
+      icon: `<i class="fas fa-hand-scissors"></i>`,
     },
   ];
 
@@ -55,11 +59,32 @@ function computerSelection() {
   //   options[Math.floor(Math.random() * options.length)]);
 }
 
-// Function to check who wins
+function getWinner() {
+  if (humanImg.innerHTML === computerImg.innerHTML) {
+    alert("Draw");
+  }
+
+  if (
+    (humanImg.innerHTML === '<i class="fas fa-hand-rock"></i>' &&
+      computerImg.innerHTML === '<i class="fas fa-hand-scissors"></i>') ||
+    (humanImg.innerHTML === "scissors" && computerImg.innerHTML === "paper") ||
+    (humanImg.innerHTML === "paper" && computerImg.innerHTML === "rock")
+  ) {
+    // return "You win! 🎉";
+    alert("Human wins");
+    let newScore = scoreEl1++;
+    scoreEl1.textContent = newScore;
+  } else {
+    return "Computer wins! 🤖";
+  }
+}
+
+// to check who wins
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     humanSelection(button);
     computerSelection();
+    getWinner();
   });
 });
 
@@ -68,5 +93,5 @@ newGameBtn.addEventListener("click", () => {
   humanImg.innerHTML = "";
   computerImg.innerHTML = "";
   scoreEl1.textContent = 0;
-  scoreEl2.textContent = 4;
+  scoreEl2.textContent = 0;
 });
---
+++ rules.md
@@ -0,0 +1,4 @@
+Rock beats Scissors
+Scissors beats paper
+Paper beats rock
+If both choices are the same, it is a tie
