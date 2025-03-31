ac22f4793d9e9f6a4824981364e03c6f258f06e6
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-03-31 12:51:35 +0100

    Little change to CSS styling and javascript code

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ let scissorsEl = document.getElementById("scissors");
 let humanImg = document.querySelector(".human-image");
 let computerImg = document.querySelector(".computer-image");
 let buttons = document.querySelectorAll(".button");
-let newGameBtn = document.querySelector("#newBtn");
-let scoreEl = document.querySelectorAll(".score");
+let newGameBtn = document.getElementById("newBtn");
+let scoreEl1 = document.querySelector(".human-score");
+let scoreEl2 = document.querySelector(".computer-score");
 
 // Function for human selection
 function humanSelection(button) {
@@ -25,13 +26,33 @@ function humanSelection(button) {
 
 // Function for the computer to pick at random between rock, paper and scissors
 function computerSelection() {
-  const icons = [
-    '<i class="fas fa-hand-rock fa-3x"></i>', // Rock
-    '<i class="fas fa-hand-paper fa-3x"></i>', // Paper
-    '<i class="fas fa-hand-scissors fa-3x"></i>', // Scissors
+  // Storing the name and HTML markup of each icon in an array of object
+  const options = [
+    // '<i class="fas fa-hand-rock fa-3x"></i>', // Rock
+    // '<i class="fas fa-hand-paper fa-3x"></i>', // Paper
+    // '<i class="fas fa-hand-scissors fa-3x"></i>', // Scissors
+    {
+      name: "Rock",
+      icon: `<i class="fas fa-hand-rock fa-3x"></i>`,
+    },
+
+    {
+      name: "Paper",
+      icon: `<i class="fas fa-hand-paper fa-3x"></i>`,
+    },
+
+    {
+      name: "Scissors",
+      icon: `<i class="fas fa-hand-scissors fa-3x"></i>`,
+    },
   ];
-  return (computerImg.innerHTML =
-    icons[Math.floor(Math.random() * icons.length)]);
+
+  const randomNumber = Math.floor(Math.random() * options.length);
+  const randomIcon = (computerImg.innerHTML = options[randomNumber].icon);
+  return randomIcon;
+
+  // return (computerImg.innerHTML =
+  //   options[Math.floor(Math.random() * options.length)]);
 }
 
 // Function to check who wins
@@ -42,7 +63,10 @@ buttons.forEach((button) => {
   });
 });
 
+// Resetting the score and round counter to 0 and also the images
 newGameBtn.addEventListener("click", () => {
   humanImg.innerHTML = "";
   computerImg.innerHTML = "";
+  scoreEl1.textContent = 0;
+  scoreEl2.textContent = 4;
 });
